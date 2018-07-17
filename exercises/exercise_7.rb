47851b60a2f6c05ad920daa070c4bde8a7fa8572
Carmen Shing <carmenshing@gmail.com>
2018-07-17 20:03:16 +0000

    add validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts 'Please enter a store name'
+@store_name = gets.chomp
+puts "New store name is #{@store_name}"
+
+@new_store = Store.create(name: "#{@store_name}")
+if @new_store.errors.any?
+    @new_store.errors.each do |attribute, message|
+        puts "There is an error #{attribute}, #{message}"
+    end
+end
+
